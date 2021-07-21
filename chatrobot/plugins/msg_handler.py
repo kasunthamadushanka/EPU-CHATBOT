70b07cbfa1a790076496ff89c49badf0659385ad
KASU BRO <79846047+kasunthamadushanka@users.noreply.github.com>
2021-07-21 20:34:43 +0530

    Update msg_handler.py

--- chatrobot/plugins/msg_handler.py
+++ chatrobot/plugins/msg_handler.py
@@ -39,7 +39,7 @@ async def all_messages_catcher(event):
                 )
             )
         except telethon.errors.rpcerrorlist.UserNotParticipantError:
-            await event.reply(f"**To Message My Master, Please Join My Channel. :)**",
+            await event.reply(f"**To Message  Please Join My Channel. :)**",
                              buttons = [Button.url("Join Channel", Config.JMTC_LINK)])
             return
     await event.get_sender()
